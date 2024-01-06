59d6169ade8edad0eec91845fd97979e6f4af549
Demvok <vanya.demkov@gmail.com>
2024-01-06 22:45:21 +0200

    grand fix + model saving

--- PredictorProgram.py
+++ PredictorProgram.py
@@ -1,24 +1,27 @@
 import pandas as pd 
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.regression import DecisionTreeRegressor
+from pyspark.ml.regression import DecisionTreeRegressionModel
 spark = SparkSession.builder.appName('BigMartSales').getOrCreate()
 
 
-df_stats = pd.read_csv('data/stats.csv', index_col='stat')
-Z_SCORE = df_stats.loc['z_score'].values[0]
-STD = df_stats.loc['std'].values[0]
-
-quick_fit = spark.read.csv('constants/quick_fit.csv', header=True, inferSchema=True).drop('_c0')
-toVector = ['Item_Weight', 'Item_Fat_Content', 'Item_Visibility', 'Item_Type', 'Item_MRP', 'outletAge', 'Outlet_Size', 'Outlet_Location_Type', 'Outlet_Type', 'Item_Outlet_Sales']
-featuresVector = VectorAssembler(inputCols=toVector,outputCol="featuresVector")
-quick_fit = featuresVector.transform(quick_fit)
-quick_fit = quick_fit.select('featuresVector', 'Item_Outlet_Sales')
-model = DecisionTreeRegressor(featuresCol='featuresVector', labelCol='totalSales', maxBins=2048)
-model = model.fit(quick_fit)
+model = DecisionTreeRegressionModel.load('model')
 
 def getInterval(pred):
+    df_stats = pd.read_csv('constants/stats.csv', index_col='stat')
+    Z_SCORE = df_stats.loc['z_score'].values[0]
+    STD = df_stats.loc['std'].values[0]
     return (pred - Z_SCORE*STD, pred + Z_SCORE*STD)
+
+def predict_item(params):
+   pd.DataFrame(params).T.to_csv('data/tmp.csv', index=False)
+   df_to_predict = spark.read.csv('data/tmp.csv', header=True, inferSchema=True)
+   df_to_predict = VectorAssembler(inputCols=[str(i) for i in range(9)], outputCol='featuresVector').transform(df_to_predict).select('featuresVector')
+   return model.transform(df_to_predict).collect()[0][1]
+
+params = [100, 23, 0, 5, 580, 15, 1, 2, 0]
+pred = predict_item(params)
+print(f"Interval for this item is {round(pred,3)} ±{round(MAE, 3)}, or in between {getErrorInterval(pred)}")
 
 while True:
     print('Welcome to item sales prediction, please specify work mode:')
